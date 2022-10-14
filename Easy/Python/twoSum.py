d99eb83effa9f3d81644092b193986f4cb55b00f
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-14 15:59:48 -0700

    Create twoSum.py

---
+++ Easy/Python/twoSum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Array that would be returned
+        solution = []
+        
+        # Check each index's integer and see if they
+        # add up to the target, and if so, add their\
+        # indexes to the solution array
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if((nums[i] + nums[j]) == target):
+                    solution=[i, j]
+        
+        return solution
