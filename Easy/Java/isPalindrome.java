b100ddf042d9634b796ba9eb40c69762e1e20b7d
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-28 12:27:20 -0700

    Update isPalindrome.java
    Shorter and more concise solution.

--- Easy/Java/isPalindrome.java
+++ Easy/Java/isPalindrome.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // Convert x to a string, then find its length
-        String str = String.valueOf(x);
-        int length = str.length();
+        // Convert x into a string
+        String number = Integer.toString(x);
         
-        // Use a for loop to go over half of the string,
-        // making sure that it is the same on both the
-        // front and end of the string
-        for(int i = 0; i < length / 2; i++){
-            if(str.charAt(i) != str.charAt(length - i - 1))
+        // Go over the string seeing if it is a palindrome
+        for(int i = 0; i < number.length() / 2; i++){
+            if(number.charAt(i) != number.charAt(number.length() - i - 1))
                 return false;
         }
+        
+        // The number is a palindrome, return true
         return true;
     }
 }
