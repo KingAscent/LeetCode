7aafe29bc7d5f79794a49e4e99c1da03dc6bbe51
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-28 12:17:34 -0700

    Update twoSum.java
    Added more informative comments.

--- Easy/Java/twoSum.java
+++ Easy/Java/twoSum.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // The array that will be returned
-        int[] solution = new int[2];
+        // Array to be returned
+        int[] indeces = new int[2];
         
-        // Check each index's integer and see if they
-        // add up to the target integer, and if so,
-        // add their indexes to the solution array
+        // Go over the array of numbers, seeing if elements add
+        // up to our target while not having the same indeces
         for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    solution[0] = i;
-                    solution[1] = j;
+            for(int j = i; j < nums.length; j++){
+                if(i != j && nums[i] + nums[j] == target){
+                    indeces[0] = i;
+                    indeces[1] = j;
                 }
             }
         }
-        return solution;
+        
+        // Return the array
+        return indeces;
     }
 }
