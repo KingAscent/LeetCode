38bbef841952d41f50be0d48f26a5d74b6c56f4d
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-30 11:41:28 -0700

    Create lengthOfLastWord.java

---
+++ Easy/Java/lengthOfLastWord.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        // Boolean value to keep track of when a word is found
+        // Length of the longest last word
+        boolean wordFound = false;
+        int length = 0;
+        
+        // Go over the string looking for 
+        for(int i = s.length() - 1; 0 <= i; i--){
+            if(Character.isLetter(s.charAt(i))){
+                wordFound = true;
+                length++;
+            }
+            if(wordFound && !Character.isLetter(s.charAt(i)))
+                return length;
+        }
+        
+        return length;
+    }
+}
