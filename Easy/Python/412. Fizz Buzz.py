301f58392a43d34bafd0610e8c4f06e9c8a61f10
Vin <86906764+KingAscent@users.noreply.github.com>
2022-11-22 13:47:39 -0800

    Create 412. Fizz Buzz.py

---
+++ Easy/Python/412. Fizz Buzz.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        myList = []
+
+        for i in range(1, n + 1):
+            if(i % 15 == 0):
+                myList.append("FizzBuzz")
+            elif(i % 3 == 0):
+                myList.append("Fizz")
+            elif(i % 5 == 0):
+                myList.append("Buzz")
+            else:
+                myList.append(str(i))
+
+        return myList
