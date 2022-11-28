22525ba770c26353843ce4afaa89a0c1208069af
Vin <86906764+KingAscent@users.noreply.github.com>
2022-11-27 17:10:32 -0800

    Create 551. Student Attendance Record I.java

---
+++ Easy/Java/551. Student Attendance Record I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int countAbsent = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'A')
+                countAbsent++;
+            if(i < s.length() - 2 && s.substring(i, i + 3).equals("LLL")){
+                return false;
+            }
+        }
+
+        return countAbsent < 2;
+    }
+}
