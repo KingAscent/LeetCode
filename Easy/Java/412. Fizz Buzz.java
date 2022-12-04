10f477864355fe20ecc273abc948ba8eb3b71129
Vin <86906764+KingAscent@users.noreply.github.com>
2022-12-04 11:54:55 -0800

    Update 412. Fizz Buzz.java
    Added a shorter solution that takes up less memory and has a lower runtime.

--- Easy/Java/412. Fizz Buzz.java
+++ Easy/Java/412. Fizz Buzz.java
@@ -1,19 +1,18 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> myList = new ArrayList<>();
+        List<String> fb = new ArrayList<String>();
 
         for(int i = 1; i <= n; i++){
-            if(i % 15 == 0){
-                myList.add("FizzBuzz");
-            }else if(i % 3 == 0){
-                myList.add("Fizz");
-            }else if(i % 5 == 0){
-                myList.add("Buzz");
-            }else{
-                myList.add(String.valueOf(i));
-            }
+            if(i % 15 == 0)
+                fb.add("FizzBuzz");
+            else if(i % 3 == 0)
+                fb.add("Fizz");
+            else if(i % 5 == 0)
+                fb.add("Buzz");
+            else
+                fb.add(Integer.toString(i));
         }
 
-        return myList;
+        return fb;
     }
 }
