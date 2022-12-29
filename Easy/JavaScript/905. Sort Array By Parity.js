c8f7cfc159c0201b5d614f8ee629cf6c2f9c0a7c
Vin <86906764+KingAscent@users.noreply.github.com>
2022-12-29 12:55:37 -0800

    Create 905. Sort Array By Parity.js

---
+++ Easy/JavaScript/905. Sort Array By Parity.js
@@ -0,0 +1,14 @@
+var sortArrayByParity = function(nums) {
+    let index = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] % 2 == 0){
+            temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
+            index++;
+        }
+    }
+
+    return nums;
+};
