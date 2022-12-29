03842925daefdc8f37118a250ec91a0d9552699b
Vin <86906764+KingAscent@users.noreply.github.com>
2022-12-29 12:56:31 -0800

    Create 905. Sort Array By Parity.py

---
+++ Easy/Python/905. Sort Array By Parity.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        index = 0
+
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                temp = nums[index]
+                nums[index] = nums[i]
+                nums[i] = temp
+                index += 1
+
+        return nums
