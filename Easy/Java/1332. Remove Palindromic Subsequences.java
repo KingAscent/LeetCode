67dbf0c529107a69cc856b1240862b068c50a90d
Vin <86906764+KingAscent@users.noreply.github.com>
2023-01-04 13:19:18 -0800

    Create 1332. Remove Palindromic Subsequences.java

---
+++ Easy/Java/1332. Remove Palindromic Subsequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        if(s.length() == 0)
+            return 0;
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return 2;
+            }else{
+                i++;
+                j--;
+            }
+        }
+      
+        return 1;
+    }
+}
