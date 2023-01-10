e13d55177c66cca84fa5540fef28b80bf9bedea3
Vin <86906764+KingAscent@users.noreply.github.com>
2023-01-10 01:46:29 -0800

    Update 392. Is Subsequence.java
    Solution with a faster runtime(20 ms => 12 ms)

--- Easy/Java/392. Is Subsequence.java
+++ Easy/Java/392. Is Subsequence.java
@@ -8,10 +8,11 @@ class Solution {
         int sub = 0;
         
         for(int i = 0; i < t.length(); i++){
-            if(s.charAt(sub) == t.charAt(i))
+            if(s.charAt(sub) == t.charAt(i)){
                 sub++;
-            if(sub == s.length())
-                return true;
+                if(sub == s.length())
+                    return true;
+            }
         }
 
         return false;
