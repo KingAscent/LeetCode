4cf292778c9fc1f42976e085488d6a6cb1909013
Vin <86906764+KingAscent@users.noreply.github.com>
2023-01-10 01:40:55 -0800

    Create 392. Is Subsequence.java

---
+++ Easy/Java/392. Is Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() == 0)
+            return true;
+        if(t.length() < s.length())
+            return false;
+        
+        int sub = 0;
+        
+        for(int i = 0; i < t.length(); i++){
+            if(s.charAt(sub) == t.charAt(i))
+                sub++;
+            if(sub == s.length())
+                return true;
+        }
+
+        return false;
+    }
+}
