d3c0096acbb115e1fe624b77a31feb516c90d3c3
Vin <86906764+KingAscent@users.noreply.github.com>
2023-01-13 00:32:31 -0800

    Create 1979. Find Greatest Common Divisor of Array.java

---
+++ Easy/Java/1979. Find Greatest Common Divisor of Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        Arrays.sort(nums);
+        int max = nums[0];
+        int min = nums[nums.length - 1];
+        int greatest = 0;
+
+        for(int i = 1; i <= max; i++){
+            if(max % i == 0 && min % i == 0)
+                greatest = i;
+        }
+
+        return greatest;
+    }
+}
