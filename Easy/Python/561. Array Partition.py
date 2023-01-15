f1156af5592ebf879e0a8b77eccb580a35e65042
Vin <86906764+KingAscent@users.noreply.github.com>
2023-01-15 00:46:40 -0800

    Create 561. Array Partition.py

---
+++ Easy/Python/561. Array Partition.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        sum = 0
+        nums.sort()
+
+        for i in range(0, len(nums), 2):
+            sum += nums[i]
+        
+        return sum
