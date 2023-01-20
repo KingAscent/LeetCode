47b6fd5fd17f739243e8459f6f6b5816e76698c5
Vin <86906764+KingAscent@users.noreply.github.com>
2023-01-20 13:15:28 -0800

    Create 119. Pascal's Triangle II.py

---
+++ Easy/Python/119. Pascal's Triangle II.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        row = []
+
+        for i in range(rowIndex + 1):
+            row.append([])
+            for j in range(i + 1):
+                if j == 0 or i == j:
+                    row[i].append(1)
+                else:
+                    row[i].append(row[i - 1][j - 1] + row[i - 1][j])
+
+
+        return row[rowIndex]
