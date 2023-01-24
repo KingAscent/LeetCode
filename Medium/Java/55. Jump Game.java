d9345afd4cfb6c2a0676fa5a371230bf1f0ca51a
Vin <86906764+KingAscent@users.noreply.github.com>
2023-01-24 12:57:34 -0800

    Create 55. Jump Game.java

---
+++ Medium/Java/55. Jump Game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        // Initialize a jump value
+        int jump = 0;
+
+        // Use a for loop to check how far each index alows us to jump
+        // And see if we are able to jump over each zero
+        for(int i = 0; i < nums.length; i++){
+            if(jump < i)
+                // We cannot jump over the zero, return false
+                return false;
+            jump = Math.max(jump, i + nums[i]);
+        }
+
+        // All zeros are able to be jumped over
+        return true;
+    }
+}
