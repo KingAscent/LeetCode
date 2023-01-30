cdafc2a7aa55ebab6c65c6b8e042f6c00a14c59a
Vin <86906764+KingAscent@users.noreply.github.com>
2023-01-30 15:59:35 -0800

    Create 1137. N-th Tribonacci Number.java

---
+++ Medium/Java/1137. N-th Tribonacci Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int tribonacci(int n) {
+        // Restrictions of n
+        if(n < 3){
+            if(n == 2)
+                return 1;
+            return n;
+        }
+
+        // Initialize array to contain all the values of the Tribonacci sequence\
+        int[] tri = new int[n + 1];
+        tri[0] = 0;
+        tri[1] = 1;
+        tri[2] = 1;
+
+        // Calculate the nth value of the Tribonacci sequence by filling the array
+        for(int i = 3; i <= n; i++){
+            tri[i] = tri[i - 1] + tri[i - 2] + tri[i - 3];
+        }
+
+        // Return the nth value of the Tribonacci sequence
+        return tri[n];
+    }
+}
