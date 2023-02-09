27e4d0ff2e7a1e2dd0ee29419f064fd5b8c27650
Vin <86906764+KingAscent@users.noreply.github.com>
2023-02-09 15:42:29 -0800

    Create 80. Remove Duplicates from Sorted Array II.java

---
+++ Medium/Java/80. Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length < 3)
+            return nums.length;
+        
+        int i = 2;
+        for(int j = i; j < nums.length; j++){
+            if(nums[i - 2] != nums[j]){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
