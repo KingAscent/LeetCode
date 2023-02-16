4d5d5f172df6e38a249d6de7c66c03d375fb86f5
Vin <86906764+KingAscent@users.noreply.github.com>
2023-02-16 12:48:07 -0800

    Create 342. Power of Four.js

---
+++ Easy/JavaScript/342. Power of Four.js
@@ -0,0 +1,9 @@
+var isPowerOfFour = function(n) {
+    if(n < 1)
+        return false;
+    if(n == 1)
+        return true;
+    if(n % 4 != 0)
+        return false;
+    return isPowerOfFour(n / 4);
+};
