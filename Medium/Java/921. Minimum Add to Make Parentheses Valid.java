a06ee1ba787114fec04d37277b0a5bd79e239e38
Vin <86906764+KingAscent@users.noreply.github.com>
2023-03-02 02:12:28 -0800

    Create 921. Minimum Add to Make Parentheses Valid.java

---
+++ Medium/Java/921. Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int start = 0;
+        int end = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                end++;
+            }else{
+                if(end != 0){
+                    end--;
+                }else{
+                    start++;
+                }
+            }
+        }
+
+        return start + end;
+    }
+}
