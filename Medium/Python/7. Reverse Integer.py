6c0a25dc31a5e4fd82e1fff04138d5069eedfb4a
Vin <86906764+KingAscent@users.noreply.github.com>
2023-03-08 13:23:22 -0800

    Create 7. Reverse Integer.py

---
+++ Medium/Python/7. Reverse Integer.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def reverse(self, x):
+        isNeg = False
+        rev = 0
+
+        # Check if the number is negative
+        if x < 0:
+            x *= -1
+            isNeg = True
+        
+        while 0 <= x:
+            # Handle overflow
+            if 214748364 < rev:
+                return 0
+            if 10 <= x:
+                temp = x % 10
+                x /= 10
+                rev = (rev * 10) + temp
+            else:
+                rev = (rev * 10) + x
+                x = -1
+        
+        if isNeg:
+            rev *= -1
+        
+        return rev
