eef270f84b756a197a3c952f4c71a0c54d770568
Vin <86906764+KingAscent@users.noreply.github.com>
2023-03-16 15:43:27 -0700

    Create 1837. Sum of Digits in Base K.java

---
+++ Easy/Java/1837. Sum of Digits in Base K.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sumBase(int n, int k) {
+        int count = 0;
+
+        while(n != 0){
+            count += n % k;
+            n /= k;
+        }
+
+        return count;
+    }
+}
