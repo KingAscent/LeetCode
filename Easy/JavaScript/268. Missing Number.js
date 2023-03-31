900846558daf539e722a814ec75d9385183ccc0b
Vin <86906764+KingAscent@users.noreply.github.com>
2023-03-31 15:48:52 -0700

    Create 268. Missing Number.js

---
+++ Easy/JavaScript/268. Missing Number.js
@@ -0,0 +1,11 @@
+var missingNumber = function(nums) {
+    let n = nums.length;
+    let expectedSum = (n * (n + 1)) / 2;
+    let actualSum = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        actualSum += nums[i];
+    }
+
+    return expectedSum - actualSum;
+};
