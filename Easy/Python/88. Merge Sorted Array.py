07083cb383af8b9b0902c0e5ef648b1c0d976a0e
Vin <86906764+KingAscent@users.noreply.github.com>
2023-05-06 12:46:27 -0700

    Create 88. Merge Sorted Array.py

---
+++ Easy/Python/88. Merge Sorted Array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while 0 <= j:
+            if 0 <= i and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
