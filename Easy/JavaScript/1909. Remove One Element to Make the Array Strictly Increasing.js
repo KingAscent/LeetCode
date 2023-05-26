fd5e6c86215aed6e81fba8f0ad779a9d91ed6573
Vin <86906764+KingAscent@users.noreply.github.com>
2023-05-26 14:20:15 -0700

    Create 1909. Remove One Element to Make the Array Strictly Increasing.js

---
+++ Easy/JavaScript/1909. Remove One Element to Make the Array Strictly Increasing.js
@@ -0,0 +1,15 @@
+var canBeIncreasing = function(nums) {
+    let count = 0;
+
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] <= nums[i - 1]){
+            count++;
+            if(1 < count)
+                return false;
+            if(1 < i && nums[i] <= nums[i - 2])
+                nums[i] = nums[i - 1];
+        }
+    }
+
+    return true;
+};
