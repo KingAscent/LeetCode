91929ccfac1191a67f83ba0c968201223d692f28
Vin <86906764+KingAscent@users.noreply.github.com>
2023-05-28 15:55:17 -0700

    Create 2618. Check if Object Instance of Class.js

---
+++ Medium/JavaScript/2618. Check if Object Instance of Class.js
@@ -0,0 +1,5 @@
+var checkIfInstanceOf = function(obj, classFunction) {
+    if(obj == null || typeof classFunction !== 'function')
+        return false;
+    return Object(obj) instanceof classFunction;
+};
