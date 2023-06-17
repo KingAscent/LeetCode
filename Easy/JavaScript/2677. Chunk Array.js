cf864b97a8e5f1603dc9f7438a6db717d6541528
Vin <86906764+KingAscent@users.noreply.github.com>
2023-06-17 02:03:59 -0700

    Create 2677. Chunk Array.js

---
+++ Easy/JavaScript/2677. Chunk Array.js
@@ -0,0 +1,11 @@
+var chunk = function(arr, size) {
+    let chunked = [];
+    let index = 0;
+
+    while(index < arr.length){
+        chunked.push(arr.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+};
