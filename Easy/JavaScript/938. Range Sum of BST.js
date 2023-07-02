ee489a941cca27a36ddb9023acb2f3226b78ae36
Vin <86906764+KingAscent@users.noreply.github.com>
2023-07-02 13:39:05 -0700

    Create 938. Range Sum of BST.js

---
+++ Easy/JavaScript/938. Range Sum of BST.js
@@ -0,0 +1,14 @@
+var rangeSumBST = function(root, low, high) {
+    if(root == null)
+        return 0;
+    
+    let sum = 0;
+    if(low <= root.val && root.val <= high)
+        sum += root.val;
+    if(low < root.val)
+        sum += rangeSumBST(root.left, low, high);
+    if(root.val < high)
+        sum += rangeSumBST(root.right, low, high);
+
+    return sum;
+};
