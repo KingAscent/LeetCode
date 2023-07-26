1f2b0fb174d8cf1f77e9386df4257d643c6ed490
Vin <86906764+KingAscent@users.noreply.github.com>
2023-07-26 11:50:32 -0700

    Create 1822. Sign of the Product of an Array.js

---
+++ Easy/JavaScript/1822. Sign of the Product of an Array.js
@@ -0,0 +1,12 @@
+var arraySign = function(nums) {
+    let sign = 1;
+
+    for(let n of nums){
+        if(n == 0)
+            return 0;
+        if(n < 0)
+            sign *= -1;
+    }
+
+    return sign;
+};
