72ca5dfeda9a80f6e1ba9008d47c1739fe16632f
Vin <86906764+KingAscent@users.noreply.github.com>
2023-09-18 07:55:35 -0700

    Create 1539. Kth Missing Positive Number.java

---
+++ Easy/Java/1539. Kth Missing Positive Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        for(int n : arr){
+            if(n <= k)
+                k++;
+            else
+                break;
+        }
+
+        return k;
+    }
+}
