458445049eba072fc5b42b40e47beaf38e5b1105
Vin <86906764+KingAscent@users.noreply.github.com>
2023-10-12 00:48:11 -0700

    Create 2778. Sum of Squares of Special Elements.js

---
+++ Easy/JavaScript/2778. Sum of Squares of Special Elements.js
@@ -0,0 +1,10 @@
+var sumOfSquares = function(nums) {
+    let sum = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums.length % (i + 1) == 0)
+            sum += nums[i] * nums[i];
+    }
+
+    return sum;
+};
