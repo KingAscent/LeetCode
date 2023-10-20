bd03f1adc689d2ab87f6eee35dc94e228750c585
Vin <86906764+KingAscent@users.noreply.github.com>
2023-10-20 12:57:54 -0700

    Create 226. Invert Binary Tree.js

---
+++ Easy/JavaScript/226. Invert Binary Tree.js
@@ -0,0 +1,10 @@
+var invertTree = function(root) {
+    if(root == null)
+        return null
+    
+    let temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+
+    return root;
+};
