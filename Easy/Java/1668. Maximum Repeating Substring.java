c2d1c1217d47a506ba6f511748ea52bd5a075e7c
Vin <86906764+KingAscent@users.noreply.github.com>
2023-11-18 00:43:46 -0800

    Create 1668. Maximum Repeating Substring.java

---
+++ Easy/Java/1668. Maximum Repeating Substring.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int count = 0;
+        String rep = word;
+        
+        while(sequence.contains(rep)){
+            count++;
+            rep += word;
+        }
+
+        return count;
+    }
+}
